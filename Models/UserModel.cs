a6d1b4469f10eebb02c290ca1a16e641dee24119
Gustavo S. <ogustavone@gmail.com>
2025-06-02 23:22:52 -0300

    Feat: hash methods base

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,15 +1,39 @@
-// using BCrypt.Net;
-// using static BCrypt.Net.BCrypt;
+using BCrypt.Net;
+using static BCrypt.Net.BCrypt;
 
 namespace ListFlow.Models
 {
     public class UserModel : BaseEntity
     {
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; init; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        private string _passwordHash = string.Empty;
 
-        //TODO: retornar hash com BCrypt
-        // metodos GetPasswordHash e VerifyPassword
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string Password
+        {
+            get
+            {
+                return _passwordHash;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Erro: A senha não pode receber um valor nulo ou vazio.");
+                }
+                _passwordHash = GetPasswordHash(value);
+            }
+        }
+
+        public string GetPasswordHash(string cleanPassword)
+        {
+            return HashPassword(cleanPassword, 12);
+        }
+
+        public bool PasswordHashIsValid(string cleanPassword, string hashedPassword)
+        {
+            return Verify(cleanPassword, hashedPassword);
+        }
     }
 }

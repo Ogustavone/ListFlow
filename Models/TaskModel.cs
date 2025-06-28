2aa9bf3ef6b212a6ad0c6c8e299859c0acbab595
Gustavo S. <ogustavone@gmail.com>
2025-06-28 01:02:26 -0300

    add StatusType enum, update task model

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -4,6 +4,15 @@ public class TaskModel : BaseEntity
 {
     public required string Title { get; set; }
     public string Description { get; set; } = string.Empty;
-    public ETaskStatus Status { get; set; } = ETaskStatus.Backlog;
+    public StatusType Status { get; set; } = StatusType.Backlog;
     public List<string> Tags { get; set; } = [];
+}
+
+public enum StatusType
+{
+    Backlog,
+    Pending,
+    InProgress,
+    Completed,
+    Cancelled
 }
